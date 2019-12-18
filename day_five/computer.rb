5f91fd740ead8df589ca8ae714fc3b670ae84be6
Darren Eid <deid@appacademy.io>
2019-12-18 10:56:55 -0800

    move code from computer class to program class

--- day_five/computer.rb
+++ day_five/computer.rb
@@ -2,27 +2,24 @@ require_relative '../day_four/code.rb'
 require 'byebug'
 
  class IntcodeComp
+    attr_accessor :pos, :rel_base, :mem, :prog
+
     def self.read_from_file(path)
         return path if path.is_a?(Array)
         input = File.read(path).split(",").map(&:to_i) + Array.new(1000000,0)
     end
 
-    def initialize(path, robot, *input)
+    def initialize(path, program)
+        @mem = IntcodeComp.read_from_file(path)
         @pos = 0
-        @input = input
-        @output = []
         @rel_base = 0
-        @program = IntcodeComp.read_from_file(path)
-        @robot = robot
+        @prog = program
     end
 
-    def run(*input)
-        @input += input
-        while true
-            op, mod = parse(@program[@pos])
-            return [0, 99] if op == 99
-            # return execute(op, mod) if op == 4
-            # puts "Executing Code: #{op} with Modes: #{mod}"
+    def run
+        loop do
+            op, mod = parse(mem[pos])
+            break if op == 99
             execute(op, mod)
         end
     end
@@ -39,69 +36,55 @@ require 'byebug'
     def execute(op, modes)
         case modes[0]
         when 1
-            p1 = @program[@pos+1]
+            p1 = mem[pos+1]
         when 2
-            p1 = @program[@program[@pos+1]+@rel_base]
+            p1 = mem[mem[pos+1] + rel_base]
         else
-            p1 = @program[@program[@pos+1]]
+            p1 = mem[mem[pos+1]]
         end
 
         case modes[1]
         when 1
-            p2 = @program[@pos+2]
+            p2 = mem[pos+2]
         when 2
-            p2 = @program[@program[@pos+2]+@rel_base]
+            p2 = mem[mem[pos+2] + rel_base]
         else
-            p2 = @program[@program[@pos+2]]
+            p2 = mem[mem[pos+2]]
         end
 
         case op
         when 1
-            adj = (modes[2] == 2) ? (@rel_base) : 0
-            @program[@program[@pos+3] + adj] = p1 + p2
-            @pos += 4
+            adj = (modes[2] == 2) ? (rel_base) : 0
+            self.mem[mem[pos+3] + adj] = p1 + p2
+            self.pos += 4
         when 2
-            adj = (modes[2] == 2) ? (@rel_base) : 0
-            @program[@program[@pos+3] + adj] = p1*p2
-            @pos += 4
+            adj = (modes[2] == 2) ? (rel_base) : 0
+            self.mem[mem[pos+3] + adj] = p1*p2
+            self.pos += 4
         when 3
-            if @input.length == 0
-                num = @robot.detect
-                # puts "[Opcode 3] Input: #{num}"
-            else
-                num = @input.shift
-                # puts "[Opcode 3] Input: #{num}"
-            end
+            num = prog.get_input
 
-            adj = (modes[0] == 2) ? (@rel_base) : 0
-            @program[@program[@pos+1] + adj] = num
-            @pos += 2
+            adj = (modes[0] == 2) ? (rel_base) : 0
+            self.mem[mem[pos+1] + adj] = num
+            self.pos += 2
         when 4
-            # puts "[Opcode 4] Output: #{p1}"
-            @output << p1
-            if @output.length == 2
-                @robot.paint(@output.shift)
-                @robot.process(@output.shift)
-            end
-            @pos += 2
+            prog.receive_output(p1)
+            self.pos += 2
         when 5
-            @pos = (p1 == 0) ? (@pos + 3) : (p2)
+            self.pos = (p1 == 0) ? (pos + 3) : (p2)
         when 6
-            @pos = (p1 == 0) ? (p2) : (@pos + 3)
+            self.pos = (p1 == 0) ? (p2) : (pos + 3)
         when 7
-            adj = (modes[2] == 2) ? (@rel_base) : 0
-            @program[@program[@pos+3] + adj] = (p1 < p2) ? (1) : (0)
-            @pos += 4
+            adj = (modes[2] == 2) ? (rel_base) : 0
+            self.mem[mem[pos+3] + adj] = (p1 < p2) ? (1) : (0)
+            self.pos += 4
         when 8
-            adj = (modes[2] == 2) ? (@rel_base) : 0
-            @program[@program[@pos+3] + adj] = (p1 == p2) ? (1) : (0)
-            @pos += 4
+            adj = (modes[2] == 2) ? (rel_base) : 0
+            self.mem[mem[pos+3] + adj] = (p1 == p2) ? (1) : (0)
+            self.pos += 4
         when 9
-            @rel_base += p1
-            @pos += 2
+            self.rel_base += p1
+            self.pos += 2
         end
     end
  end
-
-# x = IntcodeComp.new('input.txt')
-# x.run
